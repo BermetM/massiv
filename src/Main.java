8c0a9a0c9a9ed69d68826a4dbcbe8482886a2c75
BermetM <137826341+BermetM@users.noreply.github.com>
2023-06-26 22:34:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,163 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //       System.out.println("Hello world!");
+//        int [] arr0 ={5,23};
+//        boolean[] boo ={true,false,false,true};
+//        String[] strings= {"Baikal","Aidana"};
+//        double[] doubles={43,34,43,5,4};
+//        System.out.println(strings[1]);
+        //       arr[0]= random.nextInt();
+//        arr[0] = 34;
+//        arr[1] = 27;
+//        arr[2] = 67;
+//        arr[3] = 4;
+//        arr[4] = 7;
+//        Random random = new Random();
+//        int[] arr = new int[10];
+//        int summa = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = random.nextInt(30);
+//            summa += arr[i];
+//        }
+//        System.out.println("arif:" + summa / (double) arr.length);
+//        Random random = new Random();
+//        int[] arr = new int[20];
+//         int count=0;
+//        for (int i = 0; i <100; i++) {
+//            if(i>50){
+//                System.out.println("50 chon");
+//
+//            }
+//            else{
+//                System.out.println("50 kichine");
+//            }
+//            System.out.println("arif:" + count/(double)50);
+////
+//        Random ran= new Random();
+//        int[] array = new int[20];
+//        int sumChon=0;
+//        int sumKichine=0;
+//        int chon=0;
+//        int kichine =0;
+//        double b=0;
+//        for ( int i=0 ; i < array.length; i++) {
+//
+//            array[i] = ran.nextInt(0,100);
+//            System.out.println( i + "  "+array[i]);
+//            if (array[i]>50) {
+//                sumChon += array[i];
+//                chon++;
+//                System.out.println("50don chon");
+//            }
+//            else{
+//                sumKichine+=array[i];
+//
+//                System.out.println("50 don kichine");
+//                kichine ++;
+//
+//
+//            }
+//            System.out.println((double) sumChon/chon);
+//            System.out.println((double)sumKichine/kichine);
+
+        //       }
+        //  }
+        //Howwork!
+
+
+//        int[] arr={1,2,3,4,5,6,7,8,9,10};
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//
+//           if(arr[i]%2==0){
+//            System.out.println(arr[i]);
+//        }
+//    }
+
+
+        //   int[]arr1 = new int[]{2,3};
+        //  int[]arr2={2,3,4,6,8,9,56,77,889};
+
+        //Praktika!!!
+//        Random ran = new Random();
+//        int[] array = new int[15];
+//        int count=0;
+//        int count1=0;
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = ran.nextInt(1, 20);
+//            if (array[i] % 2 == 0) {
+//                count++;
+//
+//            } else if (array[i]%2!=0) {
+//                count1++;
+//
+//            }
+//        }  System.out.println("jup sandardyn esebi = "+count);
+//        System.out.println("tak sandardyn esebi = " + count1);
+//        Random ran = new Random();
+//        int[] array = new int[20];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = ran.nextInt(1,20);
+//            System.out.print(array[i] + " ");
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0);
+//            array[i] = 0;
+//
+//            Random ran = new Random();
+//            int[] Mas = new int[10];
+//            for (int i = 0; i < Mas.length; i++) {
+//                Mas[i] = (int) (Math.random() * 9 + 1);
+//                System.out.print(Mas[i] + " ");
+//            }
+//            for (int i = 0; i < Mas.length; i++) {
+//                if (Mas[i] % 2 != 0) Mas[i] = 0;
+//
+//                System.out.println(Mas[i] + "");
+//
+//        Random ran = new Random();
+//        int[] massiv =new int[20];
+//        for (int i = 1; i < massiv.length; i++) {
+//            massiv[i] = ran.nextInt(1, 20);
+//            System.out.print(massiv[i]+" ");
+//
+//        }
+//        int max=massiv[0];
+//        int min=massiv[0];
+//        double orto=0;
+//        for (int i = 0; i <massiv.length; i++) {
+//            if (max<massiv[i]){
+//                max=massiv[i];
+//            }
+//           if(min>massiv[i]){
+//               min=massiv[i];
+//           }
+//           orto+=(double) massiv[i] / massiv.length;
+//        }
+//        System.out.println();
+//        System.out.println("max :"+max);
+//        System.out.println("min :"+min);
+//        System.out.println("ortocho san :"+orto);
+
+//        maek();
+//        }
+//        static void maek(){
+//            Scanner scanner=new Scanner(System.in);
+//            String soz= scanner.nextLine();
+//            if (soz.equals("kandai")){
+//                System.out.println("lakshy ozun");
+//            }
+//            String soz1= scanner.nextLine();
+//            if (soz1.equals("jakshy rahmat")){
+//                System.out.println("kaiaktasyn");
+//            }
+//            String soz2= scanner.nextLine();
+//            if (soz2.equals("okuudamyn")){
+//                System.out.println("Maakul anda jakshy okuu!");
+//            }
+        }
+        }
